7c715457d3bd1a298badc8a47e1e095ee1ff64e3
Rocketjumper1 <164962686+Rocketjumper1@users.noreply.github.com>
2025-10-19 15:02:59 -0700

    Enhance image processing with threading and refactor viewer
    Refactor image viewer and add threading for image processing.

--- average_directory.py
+++ average_directory.py
@@ -1,16 +1,30 @@
 from PIL import Image, ImageTk
 import os
+import threading
 import time
 from image_averager import img_averager
 import tkinter as tk
 average_folder = ""
 located_in = os.path.dirname(os.path.abspath(__file__)) 
 img_size = (500, 500)
+imgs = []
+stop_event = threading.Event()
+methods = []
+first = True
 average_func_array = []
 for name, member in vars(img_averager).items():
     if isinstance(member, staticmethod):
         func = member.__func__
         average_func_array.append(func)
+def working(Event):
+    count = 4
+    while not Event.is_set():
+        print(f"working{'.' * (count % 4)}", end = "\r")
+        count += 1
+        time.sleep(0.2)
+        print("             ",end = "\r")
+        
+        
 
 def start():
     global average_folder
@@ -27,45 +41,11 @@ dir_start = f"{located_in}/{average_folder}"
 dir_list = os.listdir(dir_start)
 count = 0
 counter = 0
-def image_viewer(lis= [], dr = ""):
-    clear()
-    global R
-    global down_b
-    global up_b
-    global blur
-    global image
-    global counter
-    R.title("image viewin time")
-    print(f"directory passed is {dr}, list is {lis}")
-    Dr = f"{dr}/{lis[counter]}"
-    print(dr)
-    blur = tk.Button(R, text= "want to see all thumbnails bflurred?", command= lambda fold =dr, o_fold=f"{dr}: averaged": average_img(fold, o_fold) )
-    blur.pack(anchor="nw")
-    img = Image.open(Dr).resize(img_size)
-    tk_img = ImageTk.PhotoImage(img)
-    image= tk.Label(R, image = tk_img)
-    image.image = tk_img
-    saved_to = tk.Label(R, text=f"all images saved to {dr}")
-    image.pack()
-    def up():
-        global counter
-        if counter + 1 < len(lis):
-            counter += 1
-        image_viewer(lis, dr)
-    def down():
-        global counter
-        if counter - 1 >= 0:
-            counter -= 1
-        image_viewer(lis, dr)
-    up_b = tk.Button(R, text= "next image -->", command=up)
-    down_b = tk.Button(R, text= "<-- go back", command=down)
-    down_b.pack()
-    up_b.pack()
-    saved_to.pack()
 def average_img(folder, out_folder):
         global R
-        
-        
+        global first
+        global imgs
+        global methods
         
         print(folder)
         print(out_folder)
@@ -83,13 +63,22 @@ def average_img(folder, out_folder):
    
         
         
- 
-        
-        avg_img, current_method =  average_func_array[count](images, img_size)
-        
-        avg_img.save(f"{out_folder}/image {current_method}.png")
-        saved_to = tk.Label(R, text=f"image saved to {folder}/image {current_method}.png")
-        img_tk = ImageTk.PhotoImage(avg_img)
+         
+        if first:
+            for i in range(len(average_func_array)):
+                stop_event.clear()
+                threading.Thread(target=working, args=(stop_event,)).start()
+                avg_img, current_method =  average_func_array[i](images, img_size)
+                stop_event.set()
+                labe = tk.Label(R, text = f"saving {current_method} img")
+                labe.pack()
+                R.update_idletasks()
+                avg_img.save(f"{out_folder}/image {current_method}.png")
+                imgs.append(avg_img)
+                methods.append(current_method)
+            first = False
+        saved_to = tk.Label(R, text=f"image saved to {folder}/image {methods[count]}.png")
+        img_tk = ImageTk.PhotoImage(imgs[count])
         image = tk.Label(R, text="average_img", image=img_tk)
         
         
@@ -113,7 +102,7 @@ def average_img(folder, out_folder):
         go_back = tk.Button(R, text = "go back", command = helper_del_func)
         button_up = tk.Button(R, text= "Next method -->", command = helper_up)
         button_down = tk.Button(R, text = "<-- Previous_method", command = helper_down)
-        ave_type = tk.Label(R, text=current_method)
+        ave_type = tk.Label(R, text=methods[count])
         up = tk.Button(R, text = "go to next")
         go_back.pack(anchor="nw")
         image.image = img_tk
@@ -122,6 +111,53 @@ def average_img(folder, out_folder):
         button_down.pack()
         ave_type.pack()
         saved_to.pack()
+
+def image_viewer(lis= [], dr = ""):
+    clear()
+    global R
+    global down_b
+    global up_b
+    global blur
+    global image
+    global counter
+    def back_to_dir_entering():
+        global R
+        
+        clear()
+        entry = tk.Entry(R, width = 30)
+        activate = tk.Button(R, text="average this directory?", command = start)
+        entry.pack()
+        activate.pack()
+    R.title("image viewin time")
+    print(f"directory passed is {dr}, list is {lis}")
+    Dr = f"{dr}/{lis[counter]}"
+    print(dr)
+    dir_enter = tk.Button(R, text = "go back to directory entering?", command = back_to_dir_entering)
+    blur = tk.Button(R, text= "want to see all thumbnails bflurred?", command= lambda fold =dr, o_fold=f"{dr}: averaged": average_img(fold, o_fold) )
+    blur.pack(anchor="nw")
+    dir_enter.pack(anchor="ne")
+    img = Image.open(Dr).resize(img_size)
+    tk_img = ImageTk.PhotoImage(img)
+    image= tk.Label(R, image = tk_img)
+    image.image = tk_img
+    saved_to = tk.Label(R, text=f"all images saved to {dr}")
+    image.pack()
+    def up():
+        global counter
+        if counter + 1 < len(lis):
+            counter += 1
+        image_viewer(lis, dr)
+    def down():
+        global counter
+        if counter - 1 >= 0:
+            counter -= 1
+        image_viewer(lis, dr)
+    up_b = tk.Button(R, text= "next image -->", command=up)
+    down_b = tk.Button(R, text= "<-- go back", command=down)
+    down_b.pack()
+    up_b.pack()
+    saved_to.pack()
+
 R = tk.Tk()
 R.geometry("1000x1000")
 R.title("image averager by local directory (folder must be in same dir as script)")
